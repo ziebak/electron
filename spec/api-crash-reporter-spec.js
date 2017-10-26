0cc442929656c1e8582087374ff78bf42ae298cb
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-10-26 16:38:48 +0300

    Enable a couple of "crashReporter module" tests

--- spec/api-crash-reporter-spec.js
+++ spec/api-crash-reporter-spec.js
@@ -80,7 +80,7 @@ describe('crashReporter module', function () {
 
       // TODO(alexeykuzmin): Disabled during Chromium 61 upgrade.
       // It just hangs the test run. Fix it and enable.
-      xit('should send minidump when node processes crash', function (done) {
+      it('should send minidump when node processes crash', function (done) {
         if (process.env.APPVEYOR === 'True') return done()
         if (process.env.TRAVIS === 'true') return done()
 
