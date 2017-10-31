2125ee42befbb589175ca7126e26aeb781762df8
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-10-31 06:52:03 +0300

    FIXME: Remove redundant comment

--- spec/api-crash-reporter-spec.js
+++ spec/api-crash-reporter-spec.js
@@ -78,8 +78,6 @@ describe('crashReporter module', function () {
         })
       })
 
-      // TODO(alexeykuzmin): Disabled during Chromium 61 upgrade.
-      // It just hangs the test run. Fix it and enable.
       it('should send minidump when node processes crash', function (done) {
         if (process.env.APPVEYOR === 'True') return done()
         if (process.env.TRAVIS === 'true') return done()
